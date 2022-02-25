b5cefc9a1048c6fc4aa836dd06b09112e9ccfaf6
kholerohit <69260118+kholerohit@users.noreply.github.com>
2022-02-25 16:37:43 +0530

    --added comment  to

--- BookAPI/Startup.cs
+++ BookAPI/Startup.cs
@@ -28,6 +28,10 @@ namespace BookAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<IBookRepository, BookRepository>();
+            services.AddControllers().AddJsonOptions(options =>
+//             {
+//                 options.JsonSerializerOptions.PropertyNamingPolicy = null;
+//             });
             //services.AddDbContext<BookContext>(o => o.UseSqlite("Data Source=books.db"));
             services.AddDbContext<BookContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
